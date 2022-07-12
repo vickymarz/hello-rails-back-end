8cf657a94f4e1bf58c50124ddf9600ccfaaa6b5b
vickymarz <okorojiebube2@gmail.com>
2022-07-12 09:18:10 -0600

    Add greetings to database

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,4 @@ Rails.application.routes.draw do
   get '*page', to: 'static#index', constraints: ->(req) do
     !req.xhr? && req.format.html?
   end
-
-  root 'static#index'
 end
--- db/migrate/20220712145019_create_greetings.rb
+++ db/migrate/20220712145019_create_greetings.rb
@@ -5,5 +5,10 @@ class CreateGreetings < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+        Greeting.create(greeting: 'Hello, Otto')
+    Greeting.create(greeting: 'Hey, Micronauts!')
+Greeting.create(greeting: 'Holla, Student Success!')
+Greeting.create(greeting: 'Hi, Microverse!')
+Greeting.create(greeting: 'Yo, Victor!')
   end
 end
