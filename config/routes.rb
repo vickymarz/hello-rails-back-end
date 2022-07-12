37e51be9f59f760a17a623f366ebd33e38abf413
vickymarz <okorojiebube2@gmail.com>
2022-07-12 08:47:33 -0600

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'greetings/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'static/index'
+  namespace :v1, defaults: { format: 'json' } do
+    get 'greetings', to: 'greetings#index'
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
+  root 'static#index'
 end
